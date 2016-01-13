84fe44509d8bdd8dd7715984668fda72c5231bd6
chris <chris@chris.com>
2016-01-13 01:36:11 +0000

    Code cleanup

--- src/app/home/tennisService.js
+++ src/app/home/tennisService.js
@@ -1,44 +1,41 @@
 angular.module('tennis.service', [])
     .factory('tennisService', function() {
-
         var scoreMap = {
             0:"love",
             1:"fifteen",
             2:"thirty",
             3:"fourty"
         };
-
-        var player1 = 0;
-        var player2 = 0;
+        var player1Score = 0, player2Score = 0;
 
         return {
             getScore:getScore,
-            player1Score: player1Score,
-            player2Score: player2Score
+            scorePlayer1: scorePlayer1,
+            scorePlayer2: scorePlayer2
         };
 
         function getScore(){
-            if (player1 == player2){
-                return getDrawText(player1);
+            if (player1Score == player2Score){
+                return getDrawText(player1Score);
             } else if (isWon()){
                 return getWinningPlayer()+" wins";
             } else if (isAdvantage()){
                 return "advantage "+getWinningPlayer();
             }
-            return "player 1: "+getScoreText(player1)+" - player 2: "+getScoreText(player2);
+            return "player 1: "+getScoreText(player1Score)+" - player 2: "+getScoreText(player2Score);
         }
         function isWon(){
-            var difference = player1 - player2;
+            var difference = player1Score - player2Score;
             if (difference < 0) {
                 difference *= -1;
             }
-            return difference > 1 && (player1 > 3 || player2 > 3);
+            return difference > 1 && (player1Score > 3 || player2Score > 3);
         }
         function isAdvantage(){
-            return player1 == 4 || player2 == 4;
+            return player1Score == 4 || player2Score == 4;
         }
         function getWinningPlayer(){
-            if (player1 > player2){
+            if (player1Score > player2Score){
                 return "player 1";
             }
             return "player 2";
@@ -49,20 +46,20 @@ angular.module('tennis.service', [])
             }
             return scoreMap[score]+" all";
         }
-        function player1Score(){
-            if (player2 > 3){
-                player1 = 3;
-                player2 = 3;
+        function scorePlayer1(){
+            if (player2Score > 3){
+                player1Score = 3;
+                player2Score = 3;
             } else {
-                player1++;
+                player1Score++;
             }
         }
-        function player2Score(){
-            if (player1 > 3){
-                player1 = 3;
-                player2 = 3;
+        function scorePlayer2(){
+            if (player1Score > 3){
+                player1Score = 3;
+                player2Score = 3;
             } else {
-                player2++;
+                player2Score++;
             }
         }
         function getScoreText(score){
--- src/app/home/tennisService.spec.js
+++ src/app/home/tennisService.spec.js
@@ -12,98 +12,116 @@ describe( 'tennis service score calculations', function() {
   }));
 
   it( '15 0', inject( function(tennisService) {
-    tennisService.player1Score();
+    tennisService.scorePlayer1();
     expect( tennisService.getScore()).toBe("player 1: fifteen - player 2: love");
   }));
 
   it( '30 0', inject( function(tennisService) {
-    tennisService.player1Score();
-    tennisService.player1Score();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer1();
     expect( tennisService.getScore()).toBe("player 1: thirty - player 2: love");
   }));
 
   it( '40 0', inject( function(tennisService) {
-    tennisService.player1Score();
-    tennisService.player1Score();
-    tennisService.player1Score();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer1();
     expect( tennisService.getScore()).toBe("player 1: fourty - player 2: love");
   }));
 
+  it( '0 15', inject( function(tennisService) {
+    tennisService.scorePlayer2();
+    expect( tennisService.getScore()).toBe("player 1: love - player 2: fifteen");
+  }));
+
+  it( '0 30', inject( function(tennisService) {
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer2();
+    expect( tennisService.getScore()).toBe("player 1: love - player 2: thirty");
+  }));
+
+  it( '0 40', inject( function(tennisService) {
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer2();
+    expect( tennisService.getScore()).toBe("player 1: love - player 2: fourty");
+  }));
+
   it( '15 all', inject( function(tennisService) {
-    tennisService.player1Score();
-    tennisService.player2Score();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
     expect( tennisService.getScore()).toBe("fifteen all");
   }));
 
   it( '30 all', inject( function(tennisService) {
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
-    tennisService.player2Score();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
     expect( tennisService.getScore()).toBe("thirty all");
   }));
 
   it( 'deuce', inject( function(tennisService) {
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
-    tennisService.player2Score();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
     expect( tennisService.getScore()).toBe("deuce");
   }));
 
   it( 'advantage player 1', inject( function(tennisService) {
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
     expect( tennisService.getScore()).toBe("advantage player 1");
   }));
 
   it( 'advantage player 2', inject( function(tennisService) {
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player2Score();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer2();
     expect( tennisService.getScore()).toBe("advantage player 2");
   }));
 
   it( 'player 1 wins easily', inject( function(tennisService) {
-    tennisService.player1Score();
-    tennisService.player1Score();
-    tennisService.player1Score();
-    tennisService.player1Score();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer1();
     expect( tennisService.getScore()).toBe("player 1 wins");
   }));
 
   it( 'player 1 wins from advantage', inject( function(tennisService) {
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
-    tennisService.player1Score();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer1();
     expect( tennisService.getScore()).toBe("player 1 wins");
   }));
 
   it( 'player 1 deuce from advantage', inject( function(tennisService) {
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
-    tennisService.player2Score();
-    tennisService.player1Score();
-    tennisService.player2Score();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
+    tennisService.scorePlayer1();
+    tennisService.scorePlayer2();
     expect( tennisService.getScore()).toBe("deuce");
   }));
 
